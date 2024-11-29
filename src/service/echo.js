c19fa80b288f99cc32188016d5da6c0a70c6ed61
YamirN <ypejesreyemontero@gmail.com>
2024-11-29 02:08:31 -0500

    v17

--- src/service/echo.js
+++ src/service/echo.js
@@ -8,10 +8,13 @@ window.Pusher = Pusher;
 const echo = new Echo({
     broadcaster: 'pusher',
     key: 'c71bbb99156889c0a715',
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    wsHost: 'api.coordinacionbondy.com',
+    // wsPort: 6001,
+    // wssPort: 6001,
+    cluster: 'us2',
     forceTLS: true,
     disableStats: true,
-    // enabledTransports: ['ws', 'wss'],
+    enabledTransports: ['ws', 'wss'],
 
 });
 
