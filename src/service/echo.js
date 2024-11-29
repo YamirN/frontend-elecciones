ce0ffcc14fccd1e2f99aac6c44c72df559b16843
YamirN <ypejesreyemontero@gmail.com>
2024-11-28 20:38:15 -0500

    v10

--- src/service/echo.js
+++ src/service/echo.js
@@ -7,7 +7,7 @@ window.Pusher = Pusher;
 
 const echo = new Echo({
     broadcaster: 'pusher',
-    key: 'c71bbb99156889c0a715',
+    key: '1478963',
     wsHost: 'api.coordinacionbondy.com',
     wsPort: 6001,
     wssPort: 6001,
