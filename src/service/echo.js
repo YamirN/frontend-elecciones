0bbb6f8018d1c3437b25dafc1ab3cb5d6191cdcd
YamirN <ypejesreyemontero@gmail.com>
2024-11-28 21:07:34 -0500

    v13

--- src/service/echo.js
+++ src/service/echo.js
@@ -8,7 +8,7 @@ window.Pusher = Pusher;
 const echo = new Echo({
     broadcaster: 'pusher',
     key: 'c71bbb99156889c0a715',
-    wsHost: 'ws-us3.pusher.com',
+    wsHost: 'api.coordinacionbondy.com',
     wsPort: 443,
     wssPort: 443,
     forceTLS: true,
