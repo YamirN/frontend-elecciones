d02ff0c65876abe4a278be35d61948b7bd43de2e
YamirN <ypejesreyemontero@gmail.com>
2024-11-29 02:13:54 -0500

    v18

--- src/service/echo.js
+++ src/service/echo.js
@@ -8,13 +8,13 @@ window.Pusher = Pusher;
 const echo = new Echo({
     broadcaster: 'pusher',
     key: 'c71bbb99156889c0a715',
-    wsHost: 'api.coordinacionbondy.com',
+    // wsHost: 'api.coordinacionbondy.com',
     // wsPort: 6001,
     // wssPort: 6001,
     cluster: 'us2',
     forceTLS: true,
     disableStats: true,
-    enabledTransports: ['ws', 'wss'],
+    // enabledTransports: ['ws', 'wss'],
 
 });
 
