fd61029fc860cf222e0a068ac137da81e2e4b239
YamirN <ypejesreyemontero@gmail.com>
2024-12-06 18:28:29 -0500

    fix error 2 module export

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-import tailwindcssPrimeui from 'tailwindcss-primeui';
-
-export default {
-    darkMode: ['selector', '[class*="app-dark"]'],
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    plugins: [tailwindcssPrimeui],
-    theme: {
-        screens: {
-            sm: '576px',
-            md: '768px',
-            lg: '992px',
-            xl: '1200px',
-            '2xl': '1920px'
-        }
+export const darkMode = ['selector', '[class*="app-dark"]'];
+export const content = ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'];
+export const plugins = [require('tailwindcss-primeui')];
+export const theme = {
+    screens: {
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        '2xl': '1920px'
     }
 };
