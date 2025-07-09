c1a5cf584bf505c647cdcb1b2c02147b30ad9f1b
alexander yamir pejesreye montero <ypejesreyemontero@gmail.com>
2025-07-09 13:55:42 -0500

    update

--- src/stores/dashboardStore.js
+++ src/stores/dashboardStore.js
@@ -16,7 +16,7 @@ export const useDashboardStore = defineStore('dashboard', {
 
             try {
                 const response = await obtenerDashboard();
-                this.dashboardData = response.data;
+                this.dashboardData = response;
                 console.log('dashboardData', response);
             } catch (err) {
                 this.error = 'No se pudo cargar el dashboard';
